883d0f8559690cb863ae34a8951315922d935547
wiirhan <wiirhan@163.com>
2023-12-15 15:40:41 +0800

    feat: 合并到一个文件中

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-mod account;
+use serde::Deserialize;
+use serde_json;
+use std::io::{self};
+
+#[derive(Debug, Deserialize)]
+struct Account {
+    user: String,
+    balance: f64,
+}
+
+fn get_account() -> io::Result<Vec<Account>> {
+    let json_content = include_str!("account_data.json");
+    let users: Vec<Account> = serde_json::from_str(&json_content)?;
+    Ok(users)
+}
 
 fn main() {
-    let account_system = account::get_account().unwrap();
+    let account_system = get_account().unwrap();
 
     println!("Initial State:");
     for account in &account_system {
